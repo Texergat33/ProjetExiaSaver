c377f2327238920a975152dc6e7eff12d36b060a
Ferreira Yohann <yohann.ferreira@viacesi.fr>
2016-12-09 16:10:40 +0100

    lecture des variables d'environnement et sélection aléatoire d'un entier entre 1 et 3

--- source/eXiaSaver.c
+++ source/eXiaSaver.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+
+extern char **environ;
 
 int rand_a_b(int a, int b){
     int c = rand()%((b+1)-a)+a;
@@ -10,7 +13,27 @@ int rand_a_b(int a, int b){
 int main(int argc, char* argv[]){
     system("clear");
     srand(time(NULL));
-    int nbraleat = rand_a_b(1, 3);
-    printf("%d\n", nbraleat);
+    char* current_directory = (char *)get_current_dir_name();
+    int rnum = rand_a_b(1, 3);
+    char* exiasaver_home = getenv("EXIASAVER_HOME");
+    if(exiasaver_home == NULL) exiasaver_home = current_directory;
+    switch(rnum){
+        case 1
+            char* exiasaver1_pbm = getenv("EXIASAVER1_PBM");
+            if(exiasaver1_pbm == NULL) exiasaver1_pbm = current_directory;
+            break;
+        case 2:
+            char* exiasaver2_pbm = getenv("EXIASAVER2_PBM");
+            if(exiasaver2_pbm == NULL) exiasaver2_pbm = current_directory;
+            char* exiasaver2_taille = getenv("EXIASAVER2_TAILLE");
+            if(exiasaver2_taille == NULL) exiasaver2_taille = "5x3";
+            char* exiasaver2_sleep = getenv("EXIASAVER2_SLEEP");
+            if(exiasaver2_sleep == NULL) exiasaver2_sleep = "10";
+            break;
+        case 3:
+            char* exiasaver3_pbm = getenv("EXIASAVER3_PBM");
+            if(exiasaver3_pbm == NULL) exiasaver3_pbm = current_directory;
+            break;
+    }
     return 0;
 }

92c21d8fa3b1686d9ef4ea2a104a5c81b52e6572
Ferreira Yohann <yohann.ferreira@viacesi.fr>
2016-12-12 22:30:49 +0100

    script de compilation modifié

--- compile.sh
+++ compile.sh
@@ -3,3 +3,4 @@
 gcc source/eXiaSaver.c source/launcher.c -o eXiaSaver
 gcc eXiaSaver1/source/eXiaSaver1.c PBM/PBM.c -o eXiaSaver1/eXiaSaver1
 gcc eXiaSaver3/source/eXiaSaver3.c -o eXiaSaver3/eXiaSaver3
+echo "Done."

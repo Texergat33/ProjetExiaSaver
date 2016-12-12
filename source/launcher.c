e916c13362db530bb68500b0ae1e7f0d6b19e48e
Ferreira Yohann <yohann.ferreira@viacesi.fr>
2016-12-12 16:37:25 +0100

    changement de branche

--- source/launcher.c
+++ source/launcher.c
@@ -11,6 +11,7 @@ pid_t create_process(void){
 }
 
 int rand_a_b(int a, int b){
+    srand(time(NULL));
     int c = rand()%((b+1)-a)+a;
     return c;
 }
@@ -39,6 +40,16 @@ void execSaver1(char* arguments[], char* exiasaver_home){
     char chemin[1024];
     strcpy(chemin, exiasaver_home);
     strcat(chemin, "/eXiaSaver1/eXiaSaver1");
+    if(execv(chemin, arguments) == -1){
+        perror("execv");
+        exit(-1);
+    }
+}
+
+void execSaver3(char* arguments[], char* exiasaver_home){
+    char chemin[1024];
+    strcpy(chemin, exiasaver_home);
+    strcat(chemin, "/eXiaSaver3/eXiaSaver3");
     if(execv(chemin, arguments) == -1){
         perror("execv");
         exit(-1);
@@ -90,5 +101,20 @@ void launchExiaSaver3(char current_directory[1024], char *exiasaver_home){
     char* str = getenv("EXIASAVER3_PBM");
     if(str != NULL) strcpy(exiasaver3_pbm, str);
     else strcpy(exiasaver3_pbm, current_directory);
-    printf("Launcher 3\n");
+    char* arguments[] = {"eXiaSaver3", NULL};
+    pid_t pid = create_process();
+    switch(pid){
+        case -1:
+            perror("fork");
+            exit(-1);
+        case 0:
+            execSaver3(arguments, exiasaver_home);
+            break;
+        default:
+            if (wait(NULL) == -1) {
+                perror("wait :");
+                exit(-1);
+            }
+            break;
+    }
 }
--- source/launcher.h
+++ source/launcher.h
@@ -15,9 +15,9 @@ pid_t create_process(void);
 int rand_a_b(int, int);
 char *selectPBM(char *pbm_directory);
 void execSaver1(char* arguments[], char* exiasaver_home);
+void execSaver3(char* arguments[], char* exiasaver_home);
 void launchExiaSaver1(char current_directory[1024], char *exiasaver_home);
 void launchExiaSaver2(char current_directory[1024], char *exiasaver_home);
 void launchExiaSaver3(char current_directory[1024], char *exiasaver_home);
-
 
 #endif

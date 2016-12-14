03f51ecd411b12304433c4df678238f36feea1e9
Yohann Ferreira <yohann.ferreira@gmail.com>
2016-12-14 13:49:51 +0100

    Update dynamique.c

--- dynamique.c
+++ dynamique.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include "PBM.h"
 
-void createHour()
+PBM createBlankPBM()
 {
+    PBM pbm;
     for(int i=0; i<80, i++)
     {
         for(int j=0, j<24, j++)
         {
-            PBM.image[i][j] = " ";
+            pbm.image[i][j] = 0;
         }
     }
 }
 
-void placePBM (PBM dst, PBM source, int x, int y)
+void placePBM (PBM *dst, PBM *source, int x, int y)
 {
     for(int i = 0, i < source->length, i++)
     {
         for(int j = 0, i < source->width, j++)
         {
-            dst->image[x+i][y+j]%dst->length;
-            source->image[i][j];
+            dst->image[(x+i)%dst->length][y+j] = source->image[i][j];
         }
     }
 }

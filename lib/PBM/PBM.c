a9e6b729ed20c39e05e1ec6f68e00fd64f60ff75
Texergat33 <gatien.texereau@viacesi.fr>
2016-12-14 13:56:54 +0100

    Update PBM.c

--- lib/PBM/PBM.c
+++ lib/PBM/PBM.c
@@ -49,3 +49,26 @@ void printPBM(PBM p)
         printf("\n");
     }
 }
+
+PBM createBlankPBM()
+{
+    PBM pbm;
+    for(int i=0; i<80, i++)
+    {
+        for(int j=0, j<24, j++)
+        {
+            pbm.image[i][j] = 0;
+        }
+    }
+}
+
+void placePBM (PBM *dst, PBM *source, int x, int y)
+{
+    for(int i = 0, i < source->length, i++)
+    {
+        for(int j = 0, i < source->width, j++)
+        {
+            dst->image[(x+i)%dst->length][y+j] = source->image[i][j];
+        }
+    }
+}

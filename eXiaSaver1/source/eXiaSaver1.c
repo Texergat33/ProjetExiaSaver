1fa23bd22c06327dbc8ec996c642fc9610d7ccb2
Agathe Lagache <agathe.lagache@viacesi.fr>
2016-12-15 16:58:50 +0100

    Début de traduction de eXiaSaver1.c

--- eXiaSaver1/source/eXiaSaver1.c
+++ eXiaSaver1/source/eXiaSaver1.c
@@ -7,40 +7,40 @@ int main (int argc, char* argv[])
 {
     if(argc != 2)
     {
-        printf("Error. Incorrect number of args\n");
+        printf("Error. Incorrect number of args\n"); //print an error message if there is not the name of the opened file
         return -1;
     }
-    PBM image;
-    char key;
-    strcpy(image.name, argv[1]);
+    PBM image; //
+    char key; //create a char key which would accept keyboard inputs
+    strcpy(image.name, argv[1]); //copy the name of the PBM file into argv[1]
     for(int i=0; i<80; i++)
     {
         for(int j=0; j<24; j++)
         {
-            image.image[i][j] = 0;
+            image.image[i][j] = 0; //initialize the size of the console
         }
     }
-    char pbm_file[1024];
-    char* str = getenv("EXIASAVER1_PBM");
-    if(str != NULL) strcpy(pbm_file, str);
-    else getcwd(pbm_file, 1024);
-    strcat(pbm_file, "/");
-    strcat(pbm_file, image.name);
-    FILE* file = fopen(pbm_file, "r");
+    char pbm_file[1024]; //create a char 
+    char* str = getenv("EXIASAVER1_PBM"); //
+    if(str != NULL) strcpy(pbm_file, str); //
+    else getcwd(pbm_file, 1024); //
+    strcat(pbm_file, "/"); //add a "/" at the end of the way of the pbm file
+    strcat(pbm_file, image.name); //add the name of the pbm file after the "/"
+    FILE* file = fopen(pbm_file, "r"); //open the pbm_file for reading
     if(file != NULL)
     {
-        readResolution(file, &image);
-        loadPBM(file, &image);
-        fclose(file);
-        printPBM(image);
-        system("setterm -cursor off");
-        key = getch();
-        system("clear");
-        system("setterm -cursor on");
+        readResolution(file, &image); //
+        loadPBM(file, &image); //
+        fclose(file); //
+        printPBM(image); //
+        system("setterm -cursor off"); //remove the cursor of the screen
+        key = getch(); //read keyboard inputs without the user needs to press on entry
+        system("clear"); //erase the console
+        system("setterm -cursor on"); //puts back the cursor on the screen
     }
     else
     {
-        printf("This file can't be opened.\n");
+        printf("This file can't be opened.\n"); //print an error message
     }
     return 0;
 }
